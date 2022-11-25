69dbbaff4785357fd742e5250dea3b0bc9a19bdb
Konstantin Roppel <k.roppel@epages.com>
2022-11-25 13:00:57 +0100

    Add parameter estimation from one pair of conjugate points

--- radial_distortion/radial_distortion.py
+++ radial_distortion/radial_distortion.py
@@ -7,6 +7,27 @@ helper = importlib.util.module_from_spec(spec)
 sys.modules["helper"] = helper
 spec.loader.exec_module(helper)
 
+"""perform one of two actions on a mouse click event:
+if right-click: add coordinates of clicked point to list (params)
+if left-click: remove last point coordinates from list (params)
+
+Params:
+    event:  Type of mouse event that occurred
+    x:      X coordinate of the mouse at the moment of the occured event
+    y:      Y coordinate of the mouse at the moment of the occured event
+    flags:  - (unused)
+    params: Reference to list that contains the collected points
+"""
+def collect_calibration_points(event, x, y, flags, points):
+    # remove last item from list of points on leftclick
+    if event==cv2.EVENT_LBUTTONDOWN:
+        if len(points)!=0:
+            print(str(points.pop()) + " removed")
+    # collect point on rightclick
+    if event==cv2.EVENT_RBUTTONDOWN:
+        print((x, y))
+        points.append((x, y))
+
 def transformation_distortion(img_old, K, distortion_coefficient= -0.4):
     img_new = np.zeros_like(img_old)
     coordinates_x, coordinates_y = np.meshgrid(np.arange(1, img_old.shape[1]), np.arange(1, img_old.shape[0]))
@@ -45,6 +66,36 @@ def interpolate(img):
 
     return img_new
 
+def estimate_distortion_coefficient(img, K, P):
+    # collect points indicated by user
+    points = []
+    cv2.imshow("image", img)
+    cv2.setMouseCallback('image', collect_calibration_points, points)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+    points_3D = [np.asarray([14.5, 23, 4, 1]), ]
+    m = P.dot(points_3D[0][:,np.newaxis])
+    m /= m[-1]
+
+    C = K[:,-1]
+    K = K / C[-1]
+    C = C / C[-1]
+
+
+    alpha_u = K[0,0]
+    axis_angle = np.arccos(K[0,1]/alpha_u)
+    alpha_v = K[1,1]*np.sin(axis_angle)
+
+
+    RD_squared = np.power((m[0]-C[0]) / (alpha_u), 2) \
+            + np.power((m[1]-C[1]) / (alpha_v), 2)
+
+
+    return ((m[0]-C[0])*RD_squared*(points[0][0]-m[0])/(m[0]-C[0])*RD_squared*(m[0]-C[0])*RD_squared)
+    
+
+
 
 img_undistorted = cv2.imread('../images/scene1.jpg')
 
@@ -55,12 +106,12 @@ img_distorted = transformation_distortion(img_undistorted, K)
 img_distorted_interpolated = interpolate(img_distorted)
 
 # resize and show images
-img_undistorted = cv2.resize(img_undistorted, (int(img_undistorted.shape[1]/2), int(img_undistorted.shape[0]/2)))
-img_distorted = cv2.resize(img_distorted, (int(img_distorted.shape[1]/2), int(img_distorted.shape[0]/2)))
-img_distorted_interpolated = cv2.resize(img_distorted_interpolated, (int(img_distorted_interpolated.shape[1]/2), int(img_distorted_interpolated.shape[0]/2)))
-img_combined = np.concatenate([img_undistorted, img_distorted_interpolated], axis=1)
+img_undistorted_resize = cv2.resize(img_undistorted, (int(img_undistorted.shape[1]/2), int(img_undistorted.shape[0]/2)))
+img_distorted_interpolated_resize = cv2.resize(img_distorted_interpolated, (int(img_distorted_interpolated.shape[1]/2), int(img_distorted_interpolated.shape[0]/2)))
+img_combined = np.concatenate([img_undistorted_resize, img_distorted_interpolated_resize], axis=1)
+
+print("K estimation: " + str(estimate_distortion_coefficient(img_distorted, K, P)))
 
 cv2.imshow("images_distorted", img_combined)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
-
