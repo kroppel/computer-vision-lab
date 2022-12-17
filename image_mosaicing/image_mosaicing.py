2ce3ac436eae4e80e7daa7eb0ff30b131e3e1d43
Konstantin Roppel <k.roppel@epages.com>
2022-12-17 20:09:12 +0100

    Updated image mosaicing and helper
    -Updated image mosaicing to select specific area where keypoints are extracted
    -Add helper functions for ORB keypoint detection/matching/filtering
    -Add sample images for image mosaicing

--- image_mosaicing/image_mosaicing.py
+++ image_mosaicing/image_mosaicing.py
@@ -3,11 +3,11 @@ import cv2
 import matplotlib.pyplot as plt
 import importlib
 import sys
-
 spec = importlib.util.spec_from_file_location("", "../shared/helper.py")
 helper = importlib.util.module_from_spec(spec)
 sys.modules["helper"] = helper
 spec.loader.exec_module(helper)
+
 
 """Estimate the homography that describes the relation between a planar scene 
 shown in two images given a set of corresponding points m1, m2
@@ -21,15 +21,16 @@ Returns:
     h (np.ndarray): The homography matrix
 """
 def estimate_homography(m1, m2):
-    A = np.zeros((8,9))
+    A = np.zeros((2*len(m1),9))
+    H = None
 
     for i in np.arange(len(m1)):
         A[2*i:2*i+2,:] = np.kron(m1[i][:,np.newaxis].transpose(), 
-                    helper.calibration_direct_method.cross_product_matrix(m2[i][:,np.newaxis]))[0:2,:]
+            helper.calibration_direct_method.cross_product_matrix(m2[i][:,np.newaxis]))[0:2,:]
 
-    _, _, VH = np.linalg.svd(A)
-    H_vec = VH.transpose()[:,-1]
-    H = np.reshape(H_vec, (3,3), order='F')
+        _, _, VH = np.linalg.svd(A)
+        H_vec = VH.transpose()[:,-1]
+        H = np.reshape(H_vec, (3,3), order='F')
 
     return H
 
@@ -51,20 +52,35 @@ def perform_mosaicing_dt(imgL, imgR, H):
 
     return img_mosaicing
 
+"""Create a mosaic image using inverse transform: given two input views of a planar scene
+and the estimated homography that describes the relation between the first and the second image. 
+
+Params:
+    imgL (np.ndarray):          The first image of the planar scene
+    imgR (np.ndarray):          The second image of the planar scene
+    H (np.ndarray):             The matrix of the homography, for every point in the first image
+                                    that lies on in the planar scene should be valid, that p' = H.dot(p),
+                                    where p' is the corresponding point in the second image
+    mosaicing_shape (tuple):    The desired shape of the mosaicing image
+
+Returns:
+    img_mosaicing (np.ndarray): The mosaic image
+
+Note:
+    This function will fail if mosaicing shape is smaller that imgL.shape
+"""
 def perform_mosaicing_it(imgL, imgR, H, mosaicing_shape):
     img_mosaicing = np.zeros(mosaicing_shape)
 
     coordinates_x, coordinates_y = np.meshgrid(np.arange(0, imgL.shape[1]), np.arange(0, imgL.shape[0]))
     coordinates_mosaic_x, coordinates_mosaic_y = np.meshgrid(np.arange(0, mosaicing_shape[1]), np.arange(0, mosaicing_shape[0]))
-
-
     coordinates_mosaic_stacked = np.concatenate([coordinates_mosaic_x[:,:,np.newaxis], coordinates_mosaic_y[:,:,np.newaxis], np.ones_like(coordinates_mosaic_x)[:,:,np.newaxis]], axis=2)
 
     old_coordinates_mosaic_stacked = np.einsum("hi,jki->jkh", H, coordinates_mosaic_stacked)
     old_coordinates_mosaic_stacked /= old_coordinates_mosaic_stacked[:,:,2][:,:,np.newaxis]
 
-    coordinates_mask = np.logical_and(np.logical_and(old_coordinates_mosaic_stacked[:,:,0] >= 0, old_coordinates_mosaic_stacked[:,:,0] < img_mosaicing.shape[1]),
-                            np.logical_and(old_coordinates_mosaic_stacked[:,:,1] >= 0, old_coordinates_mosaic_stacked[:,:,1] < img_mosaicing.shape[0]))#[:,:,np.newaxis]
+    coordinates_mask = np.logical_and(np.logical_and(old_coordinates_mosaic_stacked[:,:,0] >= 0, old_coordinates_mosaic_stacked[:,:,0] < np.min((imgR.shape[1], mosaicing_shape[1]))),
+                            np.logical_and(old_coordinates_mosaic_stacked[:,:,1] >= 0, old_coordinates_mosaic_stacked[:,:,1] < np.min((imgR.shape[0], mosaicing_shape[0]))))#[:,:,np.newaxis]
 
     old_coordinates_mosaic_stacked[:,:,0] = np.clip(old_coordinates_mosaic_stacked[:,:,0], 0, imgR.shape[1]-1)
     old_coordinates_mosaic_stacked[:,:,1] = np.clip(old_coordinates_mosaic_stacked[:,:,1], 0, imgR.shape[0]-1)
@@ -77,42 +93,36 @@ def perform_mosaicing_it(imgL, imgR, H, mosaicing_shape):
     return img_mosaicing
 
 def main():
-    # Read in images and calibration parameters
-    img1 = cv2.imread('../images/panorama_image1_big.jpg')
-    img2 = cv2.imread('../images/panorama_image2_big.jpg')
+    # Read in images
+    #img1 = cv2.imread('../images/panorama_image1_big.jpg')
+    #img2 = cv2.imread('../images/panorama_image2_big.jpg')
 
+    img1 = cv2.imread('../images/bridge1.jpg')
+    img2 = cv2.imread('../images/bridge2.jpg')
+
+    # Mark area from where keypoints are extracted, then match and filter keypoints
+    number_keypoints = 20
     imgL = cv2.cvtColor(img1,cv2.COLOR_BGR2GRAY)
     imgR = cv2.cvtColor(img2,cv2.COLOR_BGR2GRAY)
+    rect_bounds = helper.get_marked_rectangle(np.copy(imgL))
+    matches, kp1, kp2 = helper.perform_orb_keypoint_matching(imgL, imgR)
+    matches_filtered = helper.filter_keypoint_matches(matches, kp1, rect_bounds, 15)
 
-    # Initiate ORB detector
-    orb = cv2.ORB_create()
-    
-    # find the keypoints and descriptors with ORB
-    kp1, des1 = orb.detectAndCompute(imgL,None)
-    kp2, des2 = orb.detectAndCompute(imgR,None)
-
-    # create BFMatcher object
-    bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
-    # Match descriptors.
-    matches = bf.match(des1,des2)
-    # Sort them in the order of their distance.
-    matches = sorted(matches, key = lambda x:x.distance)
-
-    print(len(matches))
-
-    img3 = cv2.drawMatches(imgL,kp1,imgR,kp2,matches[0:4],None,flags=cv2.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
+    img3 = cv2.drawMatches(imgL,kp1,imgR,kp2,matches_filtered[0:np.min((number_keypoints,len(matches_filtered)))],None,flags=cv2.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
     plt.imshow(img3)
     plt.show()
 
+    # Transfer 2D points into homogeneous coordinates and estimate homography
     y = []
     y_prime = []
-    for match in matches[0:4]:
+    for match in matches_filtered[0:np.min((number_keypoints,len(matches_filtered)))]:
         y.append(np.array([kp1[match.queryIdx].pt[0], kp1[match.queryIdx].pt[1], 1]))
         y_prime.append(np.array([kp2[match.trainIdx].pt[0], kp2[match.trainIdx].pt[1], 1]))
-
     H = estimate_homography(y, y_prime)
     #img_mosaicing = perform_mosaicing_dt(imgL, imgR, H)
-    img_mosaicing = perform_mosaicing_it(imgL, imgR, H, (imgL.shape[0], imgL.shape[1]+imgR.shape[1]))
+    img_mosaicing = perform_mosaicing_it(imgL, imgR, H, (imgL.shape[0], imgL.shape[1]+imgR.shape[1]-500))
+
+    print(H)
 
     plt.imshow(img_mosaicing, cmap="gray")
     plt.show()
--- shared/helper.py
+++ shared/helper.py
@@ -2,6 +2,7 @@ import cv2
 import numpy as np
 import sys
 import importlib
+import matplotlib.pyplot as plt
 
 spec = importlib.util.spec_from_file_location("", "../calibration/calibration_direct_method.py")
 calibration_direct_method = importlib.util.module_from_spec(spec)
@@ -46,6 +47,72 @@ def read_projection_matrix_from_file(path):
     f.close()
 
     return P, K, R, t
+
+"""Mark rectangle region by clicking and holding the left mouse button
+and dragging the mouse, similar to desktop behavior
+
+Params:
+    event:  Type of mouse event that occurred
+    x:      X coordinate of the mouse at the moment of the occured event
+    y:      Y coordinate of the mouse at the moment of the occured event
+    flags:  - (unused)
+    params: List containing the reference to the rectangle bounds and the input image
+"""
+def mark_rectangle(event, x, y, flags, params):
+    if event==cv2.EVENT_LBUTTONDOWN:
+        params[0][0] = (int(x),int(y))
+    elif event==cv2.EVENT_LBUTTONUP:
+        params[0][1] = (int(x),int(y))
+        cv2.rectangle(params[1],params[0][0],params[0][1],(0,255,0),2)
+
+"""Let user mark a rectangle on an image and return the rectangle boundaries 
+(upper-left and lower-right corners)
+"""
+def get_marked_rectangle(img):
+    rect_bounds = [(0,0), img.shape]
+    cv2.namedWindow('image', cv2.WINDOW_NORMAL)
+    #cv2.resizeWindow("", 520, 760)
+    cv2.setMouseCallback('image',mark_rectangle, [rect_bounds, img])
+    while(1):
+        cv2.imshow('image',img)
+        k = cv2.waitKey(1)
+        if(k == ord('q')):
+            break
+    cv2.destroyAllWindows()
+
+    return rect_bounds
+
+def perform_orb_keypoint_matching(imgL, imgR):
+    # Initiate ORB detector
+    orb = cv2.ORB_create()
+    
+    # find the keypoints and descriptors with ORB
+    kp1, des1 = orb.detectAndCompute(imgL,None)
+    kp2, des2 = orb.detectAndCompute(imgR,None)
+
+    # create BFMatcher object
+    bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
+    # Match descriptors.
+    matches = bf.match(des1,des2)
+    # Sort them in the order of their distance.
+    matches = sorted(matches, key = lambda x:x.distance)
+
+    print("Found {} matches".format(len(matches)))
+
+    return matches, kp1, kp2
+
+"""Filter given keypoint matches by x and y coordinate bounds
+"""
+def filter_keypoint_matches(matches, kp1, bounds, distance):
+    matches_filtered = []
+    for match in matches:
+        if kp1[match.queryIdx].pt[1] > bounds[0][1] and kp1[match.queryIdx].pt[1] < bounds[1][1] and \
+            kp1[match.queryIdx].pt[0] > bounds[0][0] and kp1[match.queryIdx].pt[0] < bounds[1][0] and \
+                match.distance < distance:
+            matches_filtered.append(match)
+    print("{} matches left after filtering".format(len(matches_filtered)))
+
+    return matches_filtered
 
 """Perform the direct calibration method for the two example image scenes found in the data directory
 """
